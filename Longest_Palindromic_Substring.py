7121fdaaa6d7802df2b7f5d7c9e02a9739001833
Kha Toan <nhungktdl1983@gmail.com>
2024-09-28 07:17:45 +0700

    LEARN-006

--- Longest_Palindromic_Substring.py
+++ Longest_Palindromic_Substring.py
@@ -1,16 +1,21 @@
-def is_palindrome(s: str):
+# Hàm kiểm tra 1 chuỗi có phải là xâu đối xứng
+def check_palindromic(s: str): 
     if s == s[::-1]:
         return True
     else: 
         return False
 
 def longest_palindromic_substring(s: str):
-    longest_substring = ""  # Khởi tạo chuỗi palindrome dài nhất
+    if s == "" or s == None:
+        return "Không được để chuỗi rỗng"
+    s = s.strip()
+    # Khởi tạo chuỗi palindrome dài nhất
+    longest_substring = ""  
     n = len(s)
     for i in range(n):
         for j in range(i, n):
             substring = s[i:j+1]  # Lấy chuỗi con từ i đến j
-            if is_palindrome(substring) and len(substring) > len(longest_substring) :
+            if check_palindromic(substring) and len(substring) > len(longest_substring) :
                 longest_substring = substring
 
     return    longest_substring 
--- unitest cho Longest Palindromic Substring.py
+++ unitest cho Longest Palindromic Substring.py
@@ -6,7 +6,12 @@ def test_LongestPalindromicSubstring():
     assert result == "bab" or result == "aba", "Test 1 không đạt"
 
     assert longest_palindromic_substring("cbbd") == "bb", "Test 2 không đạt"
-
+    assert longest_palindromic_substring("") == "Không được để chuỗi rỗng", "Test 3 không đạt"
+    assert longest_palindromic_substring(None) == "Không được để chuỗi rỗng", "Test 4 không đạt"
+    assert longest_palindromic_substring("madam") == "madam", "Test 5 không đạt"
+    assert longest_palindromic_substring("aaaa") == "aaaa", "Test 6 không đạt"
+    assert longest_palindromic_substring("     abbd   ") == "bb", "Test 7 không đạt" 
+    assert longest_palindromic_substring("e") == "e", "Test 8 không đạt"
     print("Tất cả test case đều đúng")
 
 test_LongestPalindromicSubstring()
