0f2891a274d6e06a0e696f334cce33dcf8246147
Kha Toan <nhungktdl1983@gmail.com>
2024-12-01 21:45:17 +0700

    LEARN-15

--- Đề 2020/ARIPOG.py
+++ Đề 2020/ARIPOG.py
@@ -1,3 +1,22 @@
+'''
+Bài 2 (5 điểm): Cấp số cộng
+Cho cấp số cộng (um) có công sai d là số nguyên khác 0. 
+Tức là: um = um-1 + d (m thuộc N*, m ≥ 2 , d  Z , d  0).
+Ví dụ: Dãy số 2, 5, 8, 11, … là một cấp số cộng với  d = 3 và u1=2.
+Yêu cầu: Cho số nguyên u1 là phần tử đầu tiên của một cấp số cộng, công sai d và một số nguyên x. Hãy cho biết x có thuộc (um) không, nếu có thì x là phần tử thứ mấy?
+Dữ liệu vào: Từ tệp văn bản ARIPOG.INP chứa ba số nguyên u1, d và x (|u1| ≤109, |x| ≤109, |d| ≤109, d  0) được ghi cách nhau bởi một dấu cách.
+Kết quả: Ghi ra tệp văn bản ARIPOG.OUT một số nguyên duy nhất là vị trí của x trong (um).  Nếu x không thuộc (um) thì ghi số -1.
+Ex:
+input: 1 3 9
+output: -1
+Giải thích: Với u1=1 và  d =3 ta có (um) là 1, 4, 7, 10, … Do đó 9 không thuộc (um).
+
+Ex:
+input: 2 -2 -6
+output: 5
+Giải thích: Với u1=2 và  d =-2 ta có (um) là  2, 0, -2, -4, -6, -8, … Do đó -6 thuộc (um) và ở vị trí thứ 5.
+'''
+
 file_input = open("C:/Users/komodo/Documents/chingchong-project/Đề 2020/ARIPOG.INP", "r")
 s1 = file_input.readline()
 A = [ int(i) for i in s1.split()]
