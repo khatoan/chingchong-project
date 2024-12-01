96bcd0060b17c8b4e50fabd8d6d707814b201a84
Kha Toan <nhungktdl1983@gmail.com>
2024-12-01 21:31:13 +0700

    LEARN-015

--- Đề 2020/ARIPOG.py
+++ Đề 2020/ARIPOG.py
@@ -4,6 +4,7 @@ A = [ int(i) for i in s1.split()]
 print(A)
 u1, d, x = A[0], A[1], A[2]
 
+#Math formula
 def main(u1, d, x):
     if (x - u1)%d ==0:
         n = (x - u1)//d+1
@@ -11,6 +12,18 @@ def main(u1, d, x):
             return n
     return -1
 
-result = main(u1, d, x)
+#Brute force
+def main_2(u1, d, x): 
+    u = u1
+    n = 1
+    while (d > 0 and u<=x) or (d < 0 and u>=x):
+        if u == x:
+            return n
+        else:
+            u += d
+            n += 1
+    return -1       
+
+result = main_2(u1, d, x)
 file_output = open("C:/Users/komodo/Documents/chingchong-project/Đề 2020/ARIPOG.OUT", "w")
 print(result, file = file_output)
