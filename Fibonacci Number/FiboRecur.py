4c7b351fd0cef30fbde3b8668294e70c7d8cd8fe
Kha Toan <nhungktdl1983@gmail.com>
2025-02-22 21:21:35 +0700

    LEARN-022

--- Fibonacci Number/FiboRecur.py
+++ Fibonacci Number/FiboRecur.py
@@ -2,14 +2,14 @@
 def fibo(n, memo):
     if memo[n] != None:
         return memo[n]
-    if n == 0:
-        return 0
-    if n == 1:
-        return 1
+    if n <= 1:
+        return n
     memo[n] = fibo(n - 1, memo) + fibo(n - 2, memo)
     return memo[n]
 
 
 def fiboRecur(n):
+    if not isinstance(n, int) or n < 0:
+        raise ValueError("Input must be a positive integer")
     memo = [None] * (n + 1)
     return fibo(n, memo)
